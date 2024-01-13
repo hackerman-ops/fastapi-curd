a66c895cf357ae168bc49b57a5462f0646351056
吴国斌 <vincent_wu2014@hotmail.com>
2024-01-13 21:09:53 +0800

    fix

--- migrations/env.py
+++ migrations/env.py
@@ -18,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from test_scripts.models import SQLModel
+from db_manager.db_models import SQLModel
 target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
