d4f18caf44149dbc6b3a28484e08fbf46429daab
吴国斌 <vincent_wu2014@hotmail.com>
2024-01-22 10:51:39 +0800

    format

--- migrations/env.py
+++ migrations/env.py
@@ -18,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from db_manager.db_models import SQLModel
+from models.db_models import SQLModel
 
 target_metadata = SQLModel.metadata
 
